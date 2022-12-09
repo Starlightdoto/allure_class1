5a3ac514e07dae0e2800454337c424bd5fa56239
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2022-12-10 01:43:58 +0600

    first commit

---
+++ src/test/java/qa/guru/allure/SelenideTest.java
@@ -0,0 +1,26 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#801")).should(exist);
+    }
+}
